70edebbc56a1ceacccac6a89e32b049d4ee011f1
Tigerball890 <littlelnu2@yahoo.com>
2016-02-27 14:11:45 -0500

    Deleted web for privacy and added package.son

--- Web/README.md
+++
@@ -1,3 +0,0 @@
-# GES-Website
-This is a website created by our family. If you want to check out our site now, you are out of luck. It is in DEVELOPMENT!!
-Which means JS, HTML and maybe... more :D
--- Web/js/ButtonScript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#addnum').click(function() {
-		console.log("I got this far! Yay!")
-		var n1 = prompt("Please type a number.")
-		var n2 = prompt("Please type another number.")
-		var sum = parseInt(n1) + parseInt(n2)
-		window.alert("Your number is: " + sum)
-	});
-});
